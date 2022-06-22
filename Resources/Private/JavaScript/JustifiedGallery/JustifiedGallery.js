b13b037bc3f52d9d285f5567bd474cf0bed5082c
justinISP <60730331+justinISP@users.noreply.github.com>
2022-06-22 13:21:33 +0200

    value changes

--- Resources/Private/JavaScript/JustifiedGallery/JustifiedGallery.js
+++ Resources/Private/JavaScript/JustifiedGallery/JustifiedGallery.js
@@ -1,8 +1,8 @@
 $(".theme-justified").justifiedGallery(
     {
         selector:       '> figure > a, > div:not(.spinner)',
-        rowHeight:      350,
-        maxRowHeight:   500,
+        rowHeight:      200,
+        maxRowHeight:   400,
         captions:       true,
         margins:        5,
         lastRow:        'justify'
